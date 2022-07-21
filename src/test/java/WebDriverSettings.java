4c6dd4c97bdc4f2c01a2806fe6b13e807e4572d5
Yauhen Shviatsou <demum1930@gmail.com>
2022-07-21 18:23:40 +0300

    add page object and price check

--- src/test/java/WebDriverSettings.java
+++ src/test/java/WebDriverSettings.java
@@ -1,6 +1,8 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Before;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -13,8 +15,18 @@ public class WebDriverSettings {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
     }
+
     @After
     public void closeConnection() {
         driver.quit();
+    }
+
+    public boolean isDisplayed(By locator) {
+        try {
+            return driver.findElement(locator).isDisplayed();
+        } catch (NoSuchElementException e) {
+            System.out.println("Элемент: " + locator + "На странице не найден");
+            return false;
+        }
     }
 }
